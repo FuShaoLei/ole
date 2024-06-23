f1541b1bc1fad5f2462b49afb11c5dd966788f8c
fushaolei <1563250958@qq.com>
2024-06-24 01:26:35 +0800

    add 添加测试模块，以后测试方便多了

---
+++ src/test/ole/core/MainTest.java
@@ -0,0 +1,13 @@
+package ole.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        Main.main(new String[]{"-sp=D:\\code\\test\\ole-test"}); // D:\code\test\ole-test
+    }
+}
